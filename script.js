b683db57abbd26928830e65d1b29ebee2fedb3ed
jumozaga <jumozaga@yahoo.com>
2023-07-15 17:45:28 -0300

    Reorganizando as coisas

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 const audio = document.getElementById('themeSong');
 const playPauseBtn = document.getElementById('playPauseBtn');
-// const audio = new Audio();
-// audio.src = "./themesong.mp3";
-
-audio.play();
-playPauseBtn.classList.remove('paused');
-playPauseBtn.classList.add('playing');
 
 
 playPauseBtn.addEventListener('click', function () {
